1a9eb895c336f67487d75201f50d71fbec86c8dc
Habtamu Bogale <abubogale342@gmail.com>
2024-05-02 22:13:16 +0300

    added dotenv package

--- src/config.js
+++ src/config.js
@@ -1,22 +1,28 @@
-import packageJson from '../package.json';
+import packageJson from "../package.json";
+import dotenv from "dotenv";
+
+// Initiate dotenv to interact with .env file values
+dotenv.config();
 
 /**
  * Pattern for config is:
  * key: process.env['KEY'] ?? default
  */
+console.log(process.env.PORT);
+
 const config = {
-    version: packageJson.version,
-    name: packageJson.name,
-    description: packageJson.description,
+  version: packageJson.version,
+  name: packageJson.name,
+  description: packageJson.description,
 
-    nodeEnv: process.env['NODE_ENV'] ?? 'development',
-    port: process.env['PORT'] ?? 3000,
+  nodeEnv: process.env["NODE_ENV"] ?? "development",
+  port: process.env["PORT"] ?? 3000,
 
-    clientOrigins: {
-        'test': process.env['DEV_ORIGIN'] ?? '*',
-        'development': process.env['DEV_ORIGIN'] ?? '*',
-        'production': process.env['PROD_ORIGIN'] ?? 'none'
-    }
-}
+  clientOrigins: {
+    test: process.env["DEV_ORIGIN"] ?? "*",
+    development: process.env["DEV_ORIGIN"] ?? "*",
+    production: process.env["PROD_ORIGIN"] ?? "none",
+  },
+};
 
-export default config
+export default config;
